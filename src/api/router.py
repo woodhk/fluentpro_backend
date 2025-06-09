a13cd834801849caea88bce0f8636d2ea3e40d3a
Alex Wood <alexwood@languagekey.com>
2025-06-09 23:57:19 +0800

    remove webhook

--- src/api/router.py
+++ src/api/router.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from .v1 import auth, users, webhooks
+from .v1 import auth, users
 
 api_router = APIRouter()
 
 # Include all v1 routes
 api_router.include_router(auth.router, prefix="/v1")
 api_router.include_router(users.router, prefix="/v1")
-api_router.include_router(webhooks.router, prefix="/v1")
