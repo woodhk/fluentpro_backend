5d83b51dab24ccd7fda2fc97361d03aab44bf1ad
Alex Wood <alexwood@languagekey.com>
2025-06-10 14:27:53 +0800

    Refactor API router to use specific auth and users routers for improved clarity and organization

--- src/api/router.py
+++ src/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from .v1 import auth, users
+from .v1.auth.auth import router as auth_router
+from .v1.users.users import router as users_router
 
 api_router = APIRouter()
 
 # Include all v1 routes
-api_router.include_router(auth.router, prefix="/v1")
-api_router.include_router(users.router, prefix="/v1")
+api_router.include_router(auth_router, prefix="/v1")
+api_router.include_router(users_router, prefix="/v1")
