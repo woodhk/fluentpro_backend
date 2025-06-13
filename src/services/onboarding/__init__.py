ec977de432936b0d5f56aa1cdbcdc26888adb51a
Alex Wood <alexwood@languagekey.com>
2025-06-13 16:33:11 +0800

    SERVICE TDD

---
+++ src/services/onboarding/__init__.py
@@ -0,0 +1,14 @@
+"""Onboarding services package."""
+from .profile_service import ProfileService
+from .job_matching_service import JobMatchingService
+from .azure_search_service import AzureSearchService
+from .communication_service import CommunicationService
+from .summary_service import OnboardingSummaryService  # Add this
+
+__all__ = [
+    "ProfileService",
+    "JobMatchingService", 
+    "AzureSearchService",
+    "CommunicationService",
+    "OnboardingSummaryService"  # Add this
+]
